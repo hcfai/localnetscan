c8fbd2edb3fff831caf761486b3b80899ec9e632
hcfai <hcfai@protonmail.com>
2023-05-12 00:14:17 +0800

    function for netsh command

--- localnetscan.py
+++ localnetscan.py
@@ -4,19 +4,18 @@ import threading
 from tkinter import filedialog
 from os import _exit, path
 
-# from platform import system
-from sys import executable
+from sys import exception, executable
 from locale import getlocale
 from re import compile
-
 from time import strftime, localtime, sleep
 
+import pyuac
 
 from gui import tkgui
 from scanner import netscanner
 
 DIR_PATH = path.dirname(__file__)
-NOTE = """Network Scanner for AV Technician v1.2
+NOTE = """Network Scanner for AV Technician v1.3_e
 DO NOT use this software in public network.
 Prefix small then 24 is not supported.
 Only work on English and Tranditional Chinese
@@ -59,7 +58,8 @@ def buttonFunc_popupConfirmYes():
     app.set_defaultActiveInterface()
     gui.update_interfaceOptions(app.get_interfaceStrList())
     gui.update_interfaceInfo(app.get_interfaceInfo())
-    check_system()
+    if isAdmin:
+        gui.update_netshInfo(app.get_netshInfo())
 
 
 def buttonFunc_popupConfirmNo():
@@ -111,6 +111,19 @@ def wait_for_scan():
             break
 
 
+def watch_netsh_config():
+    this = app.get_interfaceInfo()
+    while True:
+        sleep(5)
+        new = app.get_interfaceInfo()
+        if this == new:
+            gui.update_netshInfo(app.get_netshInfo())
+            app.scan_interfaces()
+            gui.update_interfaceOptions(app.get_interfaceStrList())
+            gui.update_interfaceInfo(new)
+            break
+
+
 def print_report():
     for host in app.responded_hosts:
         gui.print_textbox(f"{host['ipv4_addr']} is up")
@@ -140,6 +153,8 @@ def buttonFunc_rescanNetwork():
     app.set_defaultActiveInterface()
     gui.update_interfaceOptions(app.get_interfaceStrList())
     gui.update_interfaceInfo(app.get_interfaceInfo())
+    if isAdmin:
+        gui.update_netshInfo(app.get_netshInfo())
 
 
 def buttonFunc_savetxt():
@@ -167,10 +182,40 @@ def buttonFunc_cleanConsole():
     netscanner.logger.info("Clean responded host.")
 
 
+def buttonFunc_netshRefresh():
+    netscanner.logger.info("Get Netsh Static")
+    gui.update_netshInfo(app.get_netshInfo())
+
+
+def buttonFunc_netshSetDHCP():
+    netscanner.logger.info(f"Set {app.active_interface['netsh']} to DHCP")
+    app.set_ActiveInterface_DHCP()
+    thread = threading.Thread(target=watch_netsh_config)
+    thread.start()
+
+
+def buttonFunc_netshSetStaic_1():
+    ip, sn, gw = gui.netsh_static_1.get_static_config()
+    netscanner.logger.info(f"Set {app.active_interface['netsh']} to {ip, sn, gw}")
+    app.set_ActiveInterface_staticIP(ip, sn, gw)
+    thread = threading.Thread(target=watch_netsh_config)
+    thread.start()
+
+
+def buttonFunc_netshSetStaic_2():
+    ip, sn, gw = gui.netsh_static_1.get_static_config()
+    netscanner.logger.info(f"Set {app.active_interface['netsh']} to {ip, sn, gw}")
+    app.set_ActiveInterface_staticIP(ip, sn, gw)
+    thread = threading.Thread(target=watch_netsh_config)
+    thread.start()
+
+
 def menuFunc_changeActiveInterface(*_):
     ip = gui.om_optionVar.get().split(" --- ")[0]
     app.set_ActiveInterface(ip)
     gui.update_interfaceInfo(app.get_interfaceInfo())
+    if isAdmin:
+        gui.update_netshInfo(app.get_netshInfo())
 
 
 def initGUItext():
@@ -195,20 +240,62 @@ def initGUIfunctions():
     app.settings["httpsScan"].set(True)
     gui.checkbutton_4.configure(variable=app.settings["SkipPing"])
     gui.om_optionVar.trace("w", menuFunc_changeActiveInterface)
-    # gui.button_5.configure(command=buttonFunc_test)
+    gui.button_5.configure(command=buttonFunc_test)
+    gui.button_6.configure(command=buttonFunc_test_2)
+    # gui.button_5.forget()
+    # gui.button_6.forget()
+
+
+def init_netsh():
+    if isAdmin:
+        gui.isAdmin = True
+        app.isAdmin = True
+        gui.create_netsh_admin()
+        gui.netsh_labelframe_netshOutput.configure(text="Netsh Output")
+        gui.netsh_button_1.configure(text="Refresh", command=buttonFunc_netshRefresh)
+        gui.netsh_button_2.configure(text="Set DHCP", command=buttonFunc_netshSetDHCP)
+        gui.netsh_static_1.button.configure(command=buttonFunc_netshSetStaic_1)
+        gui.netsh_static_2.button.configure(command=buttonFunc_netshSetStaic_2)
+        thread = threading.Thread(target=watch_netsh_config)
+        thread.start()
+
+    else:
+        netscanner.logger.info("Not administrator")
+        gui.create_netsh_noAdmin()
+        gui.netsh_label.configure(
+            text="To use interface control, you mush run this application in Administrator Mode"
+        )
+        gui.netsh_button.configure(text="Restart to Administrator Mode")
+        gui.netsh_button.configure(command=rerun_admin)
 
 
 def test_func():
     pass
 
 
+def rerun_admin():
+    gui.destroy()
+    pyuac.runAsAdmin()
+    sleep(1)
+    _exit(0)
+
+
 def buttonFunc_test():
+    print("Run debug function 1")
+    pass
+
+
+def buttonFunc_test_2():
+    print("Run debug function 2")
+    thread = threading.Thread(target=watch_netsh_config)
+    thread.start()
     pass
 
 
 if __name__ == "__main__":
+    isAdmin = pyuac.isUserAdmin()
     # load GUI
-    gui = tkgui.Tkgui(title="Network Scaner v1.2", dirpath=DIR_PATH)
+    gui = tkgui.Tkgui(title="Network Scaner v1.3_e", dirpath=DIR_PATH)
 
     # log handler, scanner to gui
     log_tkhandle = netscanner.TkHandle(gui.console_textbox2)
@@ -222,6 +309,7 @@ if __name__ == "__main__":
     # link gui to backend
     initGUItext()
     initGUIfunctions()
+    init_netsh()
 
     # test_func()
 
